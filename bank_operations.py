8f213b48f120f06151a0e9ea244522985dae48cd
blood <denispisenkov88890@gmail.com>
2024-09-07 16:44:22 +0300

    3_rework_bank_system

--- bank_operations.py
+++ bank_operations.py
@@ -1,8 +1,7 @@
-from typing import Optional
 from sqlalchemy.orm import sessionmaker
 from config import engine, User
-
-
+from functools import wraps
+from contextlib import contextmanager
 
 Session = sessionmaker(bind=engine)
 
@@ -12,89 +11,98 @@ class BankAccount:
     def __init__(self, __balance: float, owner_full_name: str, card_number: int, card_password: int):
         self.__balance = __balance
         self.owner_full_name = owner_full_name
-        self.card_number = card_number
-        self.card_password = card_password
+        self.owner_card_number = card_number
+        self.owner_card_password = card_password
         self.valid = False
         self.add_user()
 
-
-    def validate(self, input_card_password: int) -> Optional[str]:
-        """Аунтификация по паролю"""
-        if input_card_password == self.card_password:
-            self.valid = True
-            return f'Вы успешно вошли в аккаунт'
-        elif input_card_password != self.owner_full_name: # Предупреждение если пароль неправильное
-            return f'Пароль неправильный!'
-
-
-    def deposit(self, value: float) -> Optional[str]:
-        """Пополнение баланса"""
-        if self.valid == True:
-            self.__balance += value
-            self.update_balance_in_db()
-            return f'{self.owner_full_name}, на ваш счет поступило {value}, баланс составляет {round(self.__balance, 2)}'
-        return 'Необходимо пройти аунтификацию'
-
-
-    def withdraw(self, value: float) -> Optional[str]:
-        """Вывод с баланса"""
-        if self.valid == True:
-            if value <= self.__balance:
-                self.__balance -= value
-                self.update_balance()
-                return f'{self.owner_full_name}, с вашего счета списано {value}, баланс составляет {round(self.__balance, 2)}'
-            elif value > self.__balance: # Предупреждение нехватки средств
-                return f'У вас на счете недостаточно средств для вывода!'
-        return 'Необходимо пройти аунтификацию'
-
-
-    def get_balance(self) -> Optional[str]:
-        """Отображение баланса"""
-        if self.valid == True:
-            return f"{self.owner_full_name}, ваш баланс составляет {self.__balance}"
-
-
-    def update_balance(self) -> Optional[str]:
+    @contextmanager
+    def open_session(self):
         session = Session()
         try:
-            user = session.query(User).filter_by(full_name=self.owner_full_name).first()
-            if user:
-                user.balance = self.__balance
-                session.commit()
-        except Exception:
+            yield session
+            session.commit()
+        except Exception as e:
             session.rollback()
-            print('Ошибка соединения с базой данных!')
+            return f'Ошибка соединения с базой данных!'
         finally:
             session.close()
 
 
-    def add_user(self) -> Optional[str]:
-        session = Session()
-        try:
-            user = session.query(User).filter_by(name=self.owner_full_name, card_number=self.card_number).first()
+    def authenticated(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            if not self.valid:
+                return f'Необходимо пройти аунтификацию!'
+            return func(self, *args, **kwargs)
+        return wrapper
+
+
+    def authenticate_user(self, full_name, input_card_number, input_card_password: int) -> str:
+        """Аунтификация по паролю"""
+        with self.open_session() as session:
+            user = session.query(User).filter_by(
+                name=full_name,
+                card_password=input_card_password,
+                card_number=input_card_number
+            ).first()
+            if user:
+                self.valid = True
+                return f'Вы успешно вошли в аккаунт'
+            return f'Введены неверные данные'
+
+
+    @authenticated
+    def deposit(self, value: float) -> str:
+        """Пополнение баланса"""
+        self.__balance += value
+        self.update_balance()
+        return f'{self.owner_full_name}, на ваш счет поступило {value}, баланс составляет {round(self.__balance, 2)}'
+
+    @authenticated
+    def withdraw(self, value: float) -> str:
+        """Вывод с баланса"""
+        if value <= self.__balance:
+            self.__balance -= value
+            self.update_balance()
+            return f'{self.owner_full_name}, с вашего счета списано {value}, баланс составляет {round(self.__balance, 2)}'
+        elif value > self.__balance:  # Предупреждение нехватки средств
+            return f'У вас на счете недостаточно средств для вывода!'
+
+    @authenticated
+    def get_balance(self) -> str:
+        """Отображение баланса"""
+        return f"{self.owner_full_name}, ваш баланс составляет {self.__balance}"
+
+
+    def update_balance(self) -> None:
+        with self.open_session() as session:
+            user = session.query(User).filter_by(name=self.owner_full_name, card_number=self.owner_card_number).first()
+            if user:
+                user.balance = self.__balance
+
+
+    def add_user(self) -> None:
+        with self.open_session() as session:
+            user = session.query(User).filter_by(name=self.owner_full_name, card_number=self.owner_card_number).first()
             if not user:
                 new_user = User(
                     name=self.owner_full_name,
-                    card_number=self.card_number,
-                    card_password=self.card_password,
+                    card_number=self.owner_card_number,
+                    card_password=self.owner_card_password,
                     balance=self.__balance
                 )
                 session.add(new_user)
-                session.commit()
             else:
-                return 'Такой пользователь уже существует!'
-
-        except Exception as e:
-            session.rollback()
-            print('Ошибка соединения с базоый данных:', e)
-        finally:
-            session.close()
+                return f'Такой пользователь уже существует!'
 
 
-account = BankAccount(520.0,'Денисов Денис Денисович', 52,2552)
-account.validate('Денисов Денис Денисович')
-account.get_balance()
-account.deposit(50)
-account.withdraw(700)
-account.get_balance()
-account.withdraw(73)
+
+account = BankAccount(320.0,'Иванов Денис Денисович', 522,2552)
+account.authenticate_user('Иванов Денис Денисович', 522,2552)
+#print(account.get_balance())
+print(account.deposit(523))
+print(account.get_balance())
+#print(account.withdraw(700))
+#print(account.get_balance())
+#print(account.withdraw(130))
--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import declarative_base
 
 
 engine = create_engine('postgresql://postgres:qwerty@localhost/bank_system')
